1387616c2fc56ecf80b4d0516180ca69a46d12c3
kietntxusux <kietnt.xusux@gmail.com>
2026-05-22 10:08:16 +0700

    update start script(fix lỗi không lấy được dữ liệu gainers.losers)

--- scripts/start.sh
+++ scripts/start.sh
@@ -68,8 +68,11 @@ done
 log "⚡ Submit Spark Streaming job..."
 sleep 10
 docker exec spark-master /spark/bin/spark-submit \
-  --master local[4] \
+  --master spark://spark-master:7077 \
+  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1 \
+  --conf spark.sql.shuffle.partitions=6 \
   --conf spark.sql.session.timeZone=UTC \
+  --conf spark.executor.memory=1g \
   /opt/spark-jobs/streaming/streaming_job.py > /tmp/spark-streaming.log 2>&1 &
 ok "Spark Streaming job submitted (log: /tmp/spark-streaming.log)"
 
